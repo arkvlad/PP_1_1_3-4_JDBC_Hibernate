f759e9c0cb8a4c32b18b9cffe7060897929993b5
Vlad A <arkvlad@bk.ru>
2023-07-21 16:40:29 +0300

    jdbc

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,7 @@
 package jm.task.core.jdbc;
 
-import com.mysql.cj.jdbc.Driver;
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-import java.sql.*;
-import java.util.Arrays;
 
 public class Main {
 
@@ -19,31 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-         final String testName = "Ivan";
-         final String testLastName = "Ivanov";
-         final byte testAge = 5;
-
-
         UserService userService = new UserServiceImpl();
-        userService.dropUsersTable();
-        userService.createUsersTable();
-        userService.saveUser(testName, testLastName, testAge);
-        userService.saveUser(testName, testLastName, testAge);
-        userService.saveUser(testName, testLastName, testAge);
-        userService.saveUser(testName, testLastName, testAge);
 
-        userService.cleanUsersTable();
-
-        userService.getAllUsers().forEach(System.out::println);
-        if (userService.getAllUsers().size() != 0) {
-            System.out.println("Метод очищения таблицы пользователей реализован не корректно");
-        }
-
-    }
-}
-
-
-        /*
 //        Создание таблицы User(ов)
         userService.createUsersTable();
 
@@ -64,6 +31,3 @@ public class Main {
 
     }
 }
-
-
-         */
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,8 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
-import java.sql.*;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
@@ -13,60 +10,31 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void createUsersTable() {
-        new UserDaoHibernateImpl().createUsersTable();
+        new UserDaoJDBCImpl().createUsersTable();
     }
 
     @Override
-    public void dropUsersTable() {
-        new UserDaoHibernateImpl().dropUsersTable();
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-        new UserDaoHibernateImpl().saveUser(name, lastName, age);
-    }
-
-    @Override
-    public void removeUserById(long id) {
-        new UserDaoHibernateImpl().removeUserById(id);
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        return new UserDaoHibernateImpl().getAllUsers();
-    }
-
-    @Override
-    public void cleanUsersTable() {
-        new UserDaoHibernateImpl().cleanUsersTable();
-    }
-}
-
-
-/*
-    public void createUsersTable() {
-        new UserDaoJDBCaaaaaaaaaaaaaaaaaaaaImpl().createUsersTable();
-    }
-
     public void dropUsersTable() {
         new UserDaoJDBCImpl().dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         new UserDaoJDBCImpl().saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         new UserDaoJDBCImpl().removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return new UserDaoJDBCImpl().getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         new UserDaoJDBCImpl().cleanUsersTable();
     }
 }
-
- */
