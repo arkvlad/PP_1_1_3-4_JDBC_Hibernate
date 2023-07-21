8416b188699a06b19a816b0b416a81046313bbd8
Vlad A <arkvlad@bk.ru>
2023-07-21 16:44:12 +0300

    hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,8 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
-import java.sql.*;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
@@ -41,32 +38,3 @@ public class UserServiceImpl implements UserService {
         new UserDaoHibernateImpl().cleanUsersTable();
     }
 }
-
-
-/*
-    public void createUsersTable() {
-        new UserDaoJDBCaaaaaaaaaaaaaaaaaaaaImpl().createUsersTable();
-    }
-
-    public void dropUsersTable() {
-        new UserDaoJDBCImpl().dropUsersTable();
-    }
-
-    public void saveUser(String name, String lastName, byte age) {
-        new UserDaoJDBCImpl().saveUser(name, lastName, age);
-    }
-
-    public void removeUserById(long id) {
-        new UserDaoJDBCImpl().removeUserById(id);
-    }
-
-    public List<User> getAllUsers() {
-        return new UserDaoJDBCImpl().getAllUsers();
-    }
-
-    public void cleanUsersTable() {
-        new UserDaoJDBCImpl().cleanUsersTable();
-    }
-}
-
- */
